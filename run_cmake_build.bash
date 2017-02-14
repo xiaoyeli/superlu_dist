326a4a20946200912d950b1aa61b8d77b7f44292
Sherry Li <xsli@Sherry-Lis-MacBook-Air.local>
2017-02-13 20:21:48 -0800

    Add MAKE_INC/make.ssg1, run_cmake_build.bash

--- run_cmake_build.bash
+++ run_cmake_build.bash
@@ -1,12 +1,13 @@
-#!/bin/csh
+#!/bin/bash
 
-if ( ! $?NERSC_HOST ) then
+if [ !$?NERSC_HOST ]
+then
     echo "NERSC_HOST undefined"
-else
-  if ( "$NERSC_HOST" == "edison" ) then
-    setenv PARMETIS_ROOT ~/Edison/lib/parmetis-4.0.3
+elif [ "$NERSC_HOST" == "edison" ]
+then
+    export PARMETIS_ROOT=~/Edison/lib/parmetis-4.0.3
 #    setenv PARMETIS_BUILD_DIR ${PARMETIS_ROOT}/shared-build
-    setenv PARMETIS_BUILD_DIR ${PARMETIS_ROOT}/static-build/Linux-x86_64
+    export PARMETIS_BUILD_DIR=${PARMETIS_ROOT}/static-build/Linux-x86_64
     cmake .. \
     -DUSE_XSDK_DEFAULTS=FALSE\
     -DTPL_PARMETIS_INCLUDE_DIRS="${PARMETIS_ROOT}/include;${PARMETIS_ROOT}/metis/include" \
@@ -18,11 +19,10 @@ else
 #    -DTPL_BLAS_LIBRARIES=" " \
     -DBUILD_SHARED_LIBS=OFF \
     -DCMAKE_INSTALL_PREFIX=..
-  endif
-
-  if ( "$NERSC_HOST" == "cori" ) then
-    setenv PARMETIS_ROOT ~/Cori/lib/parmetis-4.0.3
-    setenv PARMETIS_BUILD_DIR ${PARMETIS_ROOT}/shared-build
+elif [ "$NERSC_HOST" == "cori" ]
+then
+    export PARMETIS_ROOT=~/Cori/lib/parmetis-4.0.3
+    export PARMETIS_BUILD_DIR=${PARMETIS_ROOT}/shared-build
 #    setenv PARMETIS_BUILD_DIR ${PARMETIS_ROOT}/static-build/Linux-x86_64
     cmake .. \
     -DUSE_XSDK_DEFAULTS=TRUE\
@@ -33,15 +33,15 @@ else
     -DCMAKE_C_FLAGS="-std=c99 -fPIC" \
     -DCMAKE_EXE_LINKER_FLAGS="-shared" \
     -DCMAKE_INSTALL_PREFIX=..
-  endif
-endif
+fi
 
-set THISHOST=`hostname -s`
-#echo $THISHOST
-if ( "$THISHOST" == "ssg1" ) then
-  setenv PARMETIS_ROOT ~/lib/static/parmetis-4.0.3 
-  setenv PARMETIS_BUILD_DIR ${PARMETIS_ROOT}/build/Linux-x86_64
-    echo $PARMETIS_ROOT
+THISHOST=`hostname -s`
+echo "host: $THISHOST"
+if [ "$THISHOST" == "ssg1" ]
+then
+  export PARMETIS_ROOT=~/lib/static/parmetis-4.0.3 
+  export PARMETIS_BUILD_DIR=${PARMETIS_ROOT}/build/Linux-x86_64
+  echo "ParMetis root: $PARMETIS_ROOT"
   cmake .. \
     -DTPL_PARMETIS_INCLUDE_DIRS="${PARMETIS_ROOT}/include;${PARMETIS_ROOT}/metis/include" \
     -DTPL_PARMETIS_LIBRARIES="${PARMETIS_BUILD_DIR}/libparmetis/libparmetis.a;${PARMETIS_BUILD_DIR}/libmetis/libmetis.a" \
@@ -50,7 +50,7 @@ if ( "$THISHOST" == "ssg1" ) then
     -DBUILD_SHARED_LIBS=OFF \
     -DCMAKE_C_COMPILER=mpicc \
     -DCMAKE_INSTALL_PREFIX=..
-endif
+fi
 
 # make VERBOSE=1
 # make test
