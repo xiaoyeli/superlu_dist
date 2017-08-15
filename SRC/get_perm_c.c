5133fedcfb0b0fa41083fe0fe716d481080679ff
Sherry Li <xsli@Sherry-Lis-MacBook-Air.local>
2017-08-14 22:57:42 -0700

    Update README, add colamd ordering.

--- SRC/get_perm_c.c
+++ SRC/get_perm_c.c
@@ -23,6 +23,7 @@ at the top-level directory.
  */
 
 #include "superlu_ddefs.h"
+#include "colamd.h"
 
 
 void
@@ -100,6 +101,39 @@ get_metis(
     SUPERLU_FREE(b_rowind);
 #endif
     SUPERLU_FREE(perm);
+}
+
+void
+get_colamd_dist(
+	   const int m,  /* number of rows in matrix A. */
+	   const int n,  /* number of columns in matrix A. */
+	   const int nnz,/* number of nonzeros in matrix A. */
+	   int *colptr,  /* column pointer of size n+1 for matrix A. */
+	   int *rowind,  /* row indices of size nz for matrix A. */
+	   int *perm_c   /* out - the column permutation vector. */
+	   )
+{
+    int Alen, *A, i, info, *p;
+    double knobs[COLAMD_KNOBS];
+    int stats[COLAMD_STATS];
+
+    Alen = colamd_recommended(nnz, m, n);
+
+    colamd_set_defaults(knobs);
+
+    if (!(A = (int *) SUPERLU_MALLOC(Alen * sizeof(int))) )
+        ABORT("Malloc fails for A[]");
+    if (!(p = (int *) SUPERLU_MALLOC((n+1) * sizeof(int))) )
+        ABORT("Malloc fails for p[]");
+    for (i = 0; i <= n; ++i) p[i] = colptr[i];
+    for (i = 0; i < nnz; ++i) A[i] = rowind[i];
+    info = colamd(m, n, Alen, A, p, knobs, stats);
+    if ( info == FALSE ) ABORT("COLAMD failed");
+
+    for (i = 0; i < n; ++i) perm_c[p[i]] = i;
+
+    SUPERLU_FREE(A);
+    SUPERLU_FREE(p);
 }
 
 /*! \brief
@@ -472,6 +506,13 @@ get_perm_c_dist(int_t pnum, int_t ispec, SuperMatrix *A, int_t *perm_c)
 #endif
 	      break;
 
+        case (COLAMD): /* Approximate minimum degree column ordering. */
+	      get_colamd_dist(m, n, Astore->nnz, Astore->colptr, Astore->rowind,
+			      perm_c);
+#if ( PRNTlevel>=1 )
+	      printf(".. Use approximate minimum degree column ordering.\n");
+#endif
+	      break;
         case METIS_AT_PLUS_A: /* METIS ordering on A'+A */
 	      if ( m != n ) ABORT("Matrix is not square");
 	      at_plus_a_dist(n, Astore->nnz, Astore->colptr, Astore->rowind,
