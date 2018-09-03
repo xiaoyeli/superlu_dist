838288913542cdc39b0d435b695ca87a9c09be69
Xiaoye Li <xsli@lbl.gov>
2018-09-03 15:57:58 -0700

    update for building the new trisolve code.

--- SRC/superlu_dist_config.h
+++ SRC/superlu_dist_config.h
@@ -1,4 +1,12 @@
-/* #define XSDK_INDEX_SIZE 64 */
+/* superlu_dist_config.h.in */
+
+/* Enable parmetis */
+#define HAVE_PARMETIS TRUE
+
+/* enable 64bit index mode */
+/* #undef XSDK_INDEX_SIZE */
+
 #if (XSDK_INDEX_SIZE == 64)
 #define _LONGINT 1
 #endif
+
