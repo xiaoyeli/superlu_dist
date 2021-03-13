fd827dbf5d9fa9c5fae8dafce7a8d93b548b23c9
Piyush Sao <saopk@ornl.gov>
2021-03-13 17:16:38 -0500

    Adding more functions for compilation

--- SRC/TRF3dV100/lupanels.cpp
+++ SRC/TRF3dV100/lupanels.cpp
@@ -18,16 +18,16 @@ int_t upanel_t::packed2skyline(int_t *usub, double *uval)
 LUstruct_v100::LUstruct_v100(int_t nsupers,
                              int_t *isNodeInMyGrid,
                              LUstruct_t *LUstruct,
-                             gridinfo3d_t *grid3d)
+                             gridinfo3d_t *grid3d_in)
 {
 
-    
-    gridinfo_t *grid = &(grid3d->grid2d);
-    int_t iam = grid->iam;
-    int_t Pc = grid->npcol;
-    int_t Pr = grid->nprow;
-    int_t myrow = MYROW(iam, grid);
-    int_t mycol = MYCOL(iam, grid);
+    grid3d = grid3d_in;
+    grid = &(grid3d->grid2d);
+    iam = grid->iam;
+    Pc = grid->npcol;
+    Pr = grid->nprow;
+    myrow = MYROW(iam, grid);
+    mycol = MYCOL(iam, grid);
 
     int_t *xsup = LUstruct->Glu_persist->xsup;
     int_t **Lrowind_bc_ptr = LUstruct->Llu->Lrowind_bc_ptr;
@@ -56,5 +56,128 @@ LUstruct_v100::LUstruct_v100(int_t nsupers,
             uPanelVec[i] = upanel;
         }
     }
+}
 
+int_t LUstruct_v100::dSchurComplementUpdate(int_t k, lpanel_t &lpanel, upanel_t &upanel)
+{
+    int_t st_lb = 0;
+    if (myrow == krow(k))
+        st_lb = 1;
+
+    int_t nlb = lpanel.nblocks();
+    int_t nub = upanel.nblocks();
+
+#pragma omp parallel for
+    for (size_t ij = 0; ij < (nlb - st_lb) * nub; ij++)
+    {
+        /* code */
+        int_t thread_id;
+#ifdef _OPENMP
+        thread_id = omp_get_thread_num();
+#else
+        thread_id = 0;
+#endif
+
+        double *V    = bigV + thread_id * ldt * ldt;
+        int_t  ii    = ij / nub + st_lb;
+        int_t  jj    = ij % nub;
+        double alpha = 1.0;
+        double beta  = 0.0;
+        superlu_dgemm("N", "N",
+                      lpanel.nbrow(ii), upanel.nbcol(jj), supersize(k), alpha,
+                      lpanel.blkPtr(ii), lpanel.LDA(),
+                      upanel.blkPtr(jj), upanel.LDA(), beta,
+                      V, lpanel.nbrow(ii));
+
+        // now do the scatter
+        int_t ib = lpanel.gid(ii);
+        int_t jb = upanel.gid(jj);
+
+        dScatter(lpanel.nbrow(ii), upanel.nbcol(jj),
+                    ib, jb, V, lpanel.nbrow(ii),
+                    lpanel.rowList(ii), upanel.colList(jj));
+    }
+
+    return 0;
+}
+
+// should be called from an openMP region
+int_t* LUstruct_v100::computeIndirectMap(indirectMapType direction, int_t srcLen, int_t *srcVec,
+                                         int_t dstLen, int_t *dstVec)
+{
+    if (dstVec == NULL) /*uncompressed dimension*/
+    {
+        return srcVec;
+    }
+    int_t thread_id;
+#ifdef _OPENMP
+    thread_id = omp_get_thread_num();
+#else
+    thread_id = 0;
+#endif
+    int_t *dstIdx = indirect + thread_id * ldt;
+    for (int_t i = 0; i < dstLen; i++)
+    {
+        dstIdx[dstVec[i]] = i;
+    }
+
+    int_t *RCmap = (direction == ROW_MAP) ? indirectRow : indirectCol;
+    RCmap += thread_id * ldt;
+
+    for (int_t i = 0; i < srcLen; i++)
+    {
+        RCmap[i] = dstIdx[srcVec[i]];
+    }
+
+    return RCmap;
+}
+
+int_t LUstruct_v100::dScatter(int_t m, int_t n,
+                              int_t gi, int_t gj,
+                              double *Src, int_t ldsrc,
+                              int_t *srcRowList, int_t *srcColList)
+{
+
+    double *Dst;
+    int_t lddst;
+    int_t dstRowLen, dstColLen;
+    int_t *dstRowList;
+    int_t *dstColList;
+    if (gj > gi) // its in upanel
+    {
+        int li         = g2lRow(gi);
+        int lj         = uPanelVec[li].find(gj);
+            Dst        = uPanelVec[li].blkPtr(lj);
+            lddst      = supersize(gi);
+            dstRowLen  = supersize(gi);
+            dstRowList = NULL;
+            dstColLen  = uPanelVec[li].nbcol(lj);
+            dstColList = uPanelVec[li].colList(lj);
+    }
+    else
+    {
+        int lj         = g2lCol(gj);
+        int li         = lPanelVec[lj].find(gi);
+            Dst        = lPanelVec[lj].blkPtr(li);
+            lddst      = lPanelVec[lj].LDA();
+            dstRowLen  = lPanelVec[lj].nbrow(li);
+            dstRowList = lPanelVec[lj].rowList(li);
+            dstColLen  = supersize(gj);
+            dstColList = NULL;
+    }
+
+    // compute source row to dest row mapping
+    int_t *rowS2D = computeIndirectMap(ROW_MAP, m, srcRowList,
+                                       dstRowLen, dstRowList);
+    // compute source col to dest col mapping
+    int_t *colS2D = computeIndirectMap(COL_MAP, n, srcColList,
+                                       dstColLen, dstColList);
+
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            Dst[rowS2D[i] + lddst * colS2D[j]] -= Src[i + ldsrc * j];
+        }
+    }
 }
--- SRC/TRF3dV100/lupanels.hpp
+++ SRC/TRF3dV100/lupanels.hpp
@@ -44,7 +44,11 @@ public:
         return &index[LPANEL_HEADER_SIZE + nblocks() + k];
     } 
 
+    double* blkPtr(int_t k);
+    int_t LDA();
+    int_t find(int_t k);
     // for L panel I don't need any special transformation function 
+    int_t panelSolve(double* DiagBlk, int_t LDD);
 };
 
 
@@ -82,13 +86,16 @@ public:
             -index[UPANEL_HEADER_SIZE + nblocks() + k];
     }
     // row 
-    int_t* rowList(int_t k)
+    int_t* colList(int_t k)
     {
         return &index[UPANEL_HEADER_SIZE + nblocks() + k];
     } 
 
     // for U panel 
     int_t packed2skyline(int_t* usub, double* uval );
+    double* blkPtr(int_t k);
+    int_t LDA();
+    int_t find(int_t k);
 };
 
 struct LUstruct_v100
@@ -96,15 +103,46 @@ struct LUstruct_v100
 
     lpanel_t* lPanelVec;
     upanel_t* uPanelVec;
+    gridinfo3d_t* grid3d; 
+    gridinfo_t *grid;
+    int_t iam, Pc, Pr, myrow, mycol, ldt;
+    int_t* xsup; 
+    // variables for scattering ldt*THREAD_Size 
+    int_t *indirect, *indirectRow, *indirectCol; 
+    double* bigV;       // size = THREAD_Size*ldt*ldt 
 
-    // constructor
+    int_t krow(int_t k) {return k%Pr;}
+    int_t kcol(int_t k) {return k%Pc;}
+    int_t supersize(int_t k)  {return xsup[k+1]-xsup[k];}
+    int_t g2lRow(int_t k) {return k/Pr;}
+    int_t g2lCol(int_t k) {return k/Pc;}
+
+    enum indirectMapType { ROW_MAP, COL_MAP };
+    
+    /**
+    *          C O N / D E S - T R U C T O R S
+    */
     LUstruct_v100(int_t nsupers, int_t *isNodeInMyGrid,
                 LUstruct_t *LUstruct,gridinfo3d_t *grid3d);
-    int_t dSchurComplementUpdate()
+
 
     ~LUstruct_v100()
     {
         delete lPanelVec;
         delete uPanelVec;
     }
+
+    /**
+    *           Compute Functions 
+    */
+    int_t dSchurComplementUpdate(int_t k, lpanel_t& lpanel, upanel_t& upanel);
+    int_t* computeIndirectMap(indirectMapType direction, int_t srcLen, int_t *srcVec,
+                                         int_t dstLen, int_t *dstVec);
+
+    int_t dScatter(int_t m , int_t n,
+            int_t gi, int_t gj, 
+            double* V, int_t ldv,
+            int_t* srcRowList,int_t* srcColList);
+
+    
 };
