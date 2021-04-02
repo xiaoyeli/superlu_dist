af89c973a0adc1a04ed11fe9dd34341de8ebceac
Abhishek Bagusetty <abagusetty@anl.gov>
2021-04-02 00:40:16 +0000

    [SYCL] remove accidentally added build directory

--- jlse-build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /gpfs/jlse-fs0/users/abagusetty/tmp_superlu/superlu_dist
-# Build directory: /gpfs/jlse-fs0/users/abagusetty/tmp_superlu/superlu_dist/jlse-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("SRC")
-subdirs("TEST")
-subdirs("EXAMPLE")
--- jlse-build/SRC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /gpfs/jlse-fs0/users/abagusetty/tmp_superlu/superlu_dist/SRC
-# Build directory: /gpfs/jlse-fs0/users/abagusetty/tmp_superlu/superlu_dist/jlse-build/SRC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
