6b3beddc8c427afbe81dc14b0b5066830c5ec3e9
Piyush Sao <saopk@ornl.gov>
2021-05-06 16:28:13 -0400

    remove cudafreehost

--- SRC/zutil_dist.c
+++ SRC/zutil_dist.c
@@ -456,11 +456,8 @@ int zDeAllocLlu_3d(int_t n, zLUstruct_t * LUstruct, gridinfo3d_t* grid3d)
     for (i = 0; i < nbc; ++i) 
 	if ( Llu->Lrowind_bc_ptr[i] ) {
 	    SUPERLU_FREE (Llu->Lrowind_bc_ptr[i]);
-#ifdef GPU_ACC
-	    checkCuda(cudaFreeHost(Llu->Lnzval_bc_ptr[i]));
-#else
-	    SUPERLU_FREE (Llu->Lnzval_bc_ptr[i]);
-#endif
+        SUPERLU_FREE (Llu->Lnzval_bc_ptr[i]);
+
 	}
     SUPERLU_FREE (Llu->Lrowind_bc_ptr);
     SUPERLU_FREE (Llu->Lnzval_bc_ptr);
