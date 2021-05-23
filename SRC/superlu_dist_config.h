fba7e923795f00b9287b3b239a01f16da98770ea
Sherry Li <xsli@lbl.gov>
2021-05-22 19:23:15 -0700

    In CMakeLists.txt, set XSDK-ENABLE_Fortran=ON. In .gitignore, add file SRC/superlu_FortranCInterface.h. FORTRAN/: link libsuperlu_dist_fortran.so to libsuperlu_dist.so. FORTRAN/: propagate VERSION number down to the superlu_dist_fortran target.

--- SRC/superlu_dist_config.h
+++ SRC/superlu_dist_config.h
@@ -1,7 +1,7 @@
 /* superlu_dist_config.h.in */
 
 /* Enable CUDA */
-#define HAVE_CUDA TRUE
+/* #undef HAVE_CUDA */
 
 /* Enable parmetis */
 #define HAVE_PARMETIS TRUE
@@ -13,7 +13,7 @@
 /* #undef HAVE_COMBBLAS */
 
 /* enable 64bit index mode */
-#define XSDK_INDEX_SIZE 64
+/* #undef XSDK_INDEX_SIZE */
 
 #if (XSDK_INDEX_SIZE == 64)
 #define _LONGINT 1
