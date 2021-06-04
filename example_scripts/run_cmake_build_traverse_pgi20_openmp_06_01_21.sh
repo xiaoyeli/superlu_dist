6fc64290d17bf70b9448012eb5c54c2121f1d60b
Yang Liu <liuyangzhuan@gmail.com>
2021-06-04 18:07:02 -0400

    temporarily remove fortran drivers in cmake, this is to be restored later

--- example_scripts/run_cmake_build_traverse_pgi20_openmp_06_01_21.sh
+++ example_scripts/run_cmake_build_traverse_pgi20_openmp_06_01_21.sh
@@ -6,14 +6,15 @@ module purge
 export ALLINEA_FORCE_CUDA_VERSION=20.0.1
 module load cudatoolkit/11.2 pgi/20.4 openmpi/pgi-20.4/4.0.4/64 
 module load hdf5/pgi-20.4/openmpi-4.0.4/1.10.6 fftw/gcc/openmpi-4.0.4/3.3.8 anaconda ddt 
-module load cmake
+#module load cmake
 
 
-
+export PATH=/home/yl33/cmake-3.20.3/bin/:$PATH
+cmake --version
 export PARMETIS_ROOT=~/petsc_master/traverse-pgi-openmpi-199-gpucuda-branch/
 
 export CUDA_ROOT=/usr/local/cuda-11.2
-export CUDA_PATH=${CUDA_ROOT}
+#export CUDA_PATH=${CUDA_ROOT}
 rm -rf CMakeCache.txt
 rm -rf CMakeFiles
 rm -rf CTestTestfile.cmake
@@ -23,12 +24,15 @@ rm -rf DartConfiguration.tcl
 cmake .. \
 	-DTPL_PARMETIS_INCLUDE_DIRS="${PARMETIS_ROOT}/include" \
 	-DTPL_PARMETIS_LIBRARIES="${PARMETIS_ROOT}/lib/libparmetis.a;${PARMETIS_ROOT}/lib/libmetis.a" \
-	-DCUDAToolkit_LIBRARY_ROOT="${CUDA_ROOT}" \
 	-DBUILD_SHARED_LIBS=OFF \
 	-DCMAKE_C_COMPILER=mpicc \
 	-DTPL_ENABLE_CUDALIB=TRUE \
 	-DTPL_ENABLE_LAPACKLIB=TRUE \
 	-Denable_openmp:BOOL=TRUE \
+	-DCMAKE_CUDA_FLAGS="-ccbin pgc++ -D_PGIC_PRINCETON_OVERRIDE_" \
+	-DCMAKE_CUDA_HOST_COMPILER=mpicc \
+	-DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES="/usr/local/cuda-11.2/include" \
+	-DCMAKE_INCLUDE_SYSTEM_FLAG_C="-I" \
 	-DCMAKE_CXX_COMPILER=mpiCC \
 	-DCMAKE_INSTALL_PREFIX=. \
 	-DCMAKE_BUILD_TYPE=Release \
@@ -39,8 +43,12 @@ cmake .. \
         -DCMAKE_C_FLAGS="-DPRNTlevel=1 -DPROFlevel=0 -DDEBUGlevel=0 -pgf90libs"
 
 make pddrive
+make install
+
+#	-DXSDK_ENABLE_Fortran=FALSE \ 
 
 
+#	-DCUDAToolkit_LIBRARY_ROOT="${CUDA_ROOT}" \
 
 #salloc -N 1 --qos=test -t 0:30:00 --gpus=2
 
