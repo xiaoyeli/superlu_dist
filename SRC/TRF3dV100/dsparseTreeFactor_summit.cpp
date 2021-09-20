7333a3d08bf862d1ac6d114364bf4b51847d6b21
Piyush Sao <saopk@ornl.gov>
2021-09-20 16:44:32 -0400

    resizeing lookAhead window is working on CPU

--- SRC/TRF3dV100/dsparseTreeFactor_summit.cpp
+++ SRC/TRF3dV100/dsparseTreeFactor_summit.cpp
@@ -59,7 +59,6 @@ int_t LUstruct_v100::dsparseTreeFactor(
     int_t topoLvl =0;
     int_t k_st = eTreeTopLims[topoLvl];
     int_t k_end = eTreeTopLims[topoLvl + 1];
-    
     //TODO: make this asynchronous 
     for (int_t k0 = k_st; k0 < k_end; k0++)
     {
@@ -67,11 +66,10 @@ int_t LUstruct_v100::dsparseTreeFactor(
         int_t offset = 0;
         dDiagFactorPanelSolve(k, offset,dFBufs);
         donePanelSolve[k0]=1;
-        
-        
     }
 
-    for (int k0 = k_st; k0 < SUPERLU_MIN(k_end, k_st + numLA); ++k0)
+    int_t winSize =  SUPERLU_MIN(numLA/2, eTreeTopLims[1]);
+    for (int k0 = k_st; k0 < winSize; ++k0)
     {
         int_t k = perm_c_supno[k0];
         int_t offset = k0%numLA;
@@ -79,19 +77,83 @@ int_t LUstruct_v100::dsparseTreeFactor(
         {
             dPanelBcast(k, offset);
             donePanelBcast[k0] =1;
-            
         }             
-        
     }/*for (int k0 = k_st; k0 < SUPERLU_MIN(k_end, k_st + numLA); ++k0)*/
 
+    int_t k1 =0;
+    int_t winParity=0;
+    int_t halfWin = numLA/2; 
+    while(k1<nnodes)
+    {
+        for (int_t k0 = k1; k0 < SUPERLU_MIN(nnodes, k1+winSize); ++k0)
+        { 
+            int_t k = perm_c_supno[k0];
+            int_t offset = (k0-k1)%winSize;
+            if(winParity%2)
+                offset+= halfWin;   // 
+            
+            /*=======   SchurComplement Update ======*/
+            upanel_t k_upanel(UidxRecvBufs[offset], UvalRecvBufs[offset]) ;
+            lpanel_t k_lpanel(LidxRecvBufs[offset], LvalRecvBufs[offset]);
+            if (myrow == krow(k))
+                k_upanel= uPanelVec[g2lRow(k)];
+            if (mycol == kcol(k))
+                k_lpanel = lPanelVec[g2lCol(k)];
 
+            int_t k_parent = gEtreeInfo->setree[k];
+            /* Look Ahead Panel Update */
+            if(UidxSendCounts[k]>0 && LidxSendCounts[k]>0)
+                lookAheadUpdate(k,k_parent, k_lpanel,k_upanel);
+            
+            /* Look Ahead Panel Solve */
+            if(k_parent < nsupers)
+            {
+                int_t k0_parent =  myIperm[k_parent];
+                if (k0_parent > 0 && k0_parent<nnodes)
+                {
+                    localNumChildrenLeft[k0_parent]--;
+                    if (topoLvl < maxTopoLevel - 1 && !localNumChildrenLeft[k0_parent])
+                    {
+                        int_t dOffset = 0;
+                        dDiagFactorPanelSolve(k_parent, dOffset,dFBufs);
+                        donePanelSolve[k0_parent]=1;
+                    }
+                }
+            }
+            
+            /*proceed with remaining SchurComplement update */
+            if(UidxSendCounts[k]>0 && LidxSendCounts[k]>0)
+                    dSchurCompUpdateExcludeOne(k,k_parent, k_lpanel,k_upanel);
+            
+        }
+
+        k1 = k1+winSize;
+        for (int_t k0_next = k1; k0_next < SUPERLU_MIN(nnodes, k1+winSize); ++k0_next)
+        {
+            int k_next = perm_c_supno[k0_next];
+            if (!localNumChildrenLeft[k0_next])
+            {   
+                int offset_next = (k0_next-k1)%winSize; 
+                if(!(winParity%2))
+                    offset_next += halfWin; 
+                dPanelBcast(k_next, offset_next);
+                donePanelBcast[k0_next] =1;
+            }
+            else 
+            {
+                winSize = k0_next - k1;
+                break; 
+            }
+        }
+
+        winParity++;
+    }
+
+#if 0
     for (int_t topoLvl = 0; topoLvl < maxTopoLevel; ++topoLvl)
     {
-        /* code */
         int_t k_st = eTreeTopLims[topoLvl];
         int_t k_end = eTreeTopLims[topoLvl + 1];
-        // start the pipeline 
-
         for (int_t k0 = k_st; k0 < k_end; ++k0)
         {
             int_t k = perm_c_supno[k0];
@@ -123,41 +185,28 @@ int_t LUstruct_v100::dsparseTreeFactor(
                         int_t dOffset = 0;
                         dDiagFactorPanelSolve(k_parent, dOffset,dFBufs);
                         donePanelSolve[k0_parent]=1;
-                        
                     }
                 }
             }
-            
             /*proceed with remaining SchurComplement update */
             if(UidxSendCounts[k]>0 && LidxSendCounts[k]>0)
                     dSchurCompUpdateExcludeOne(k,k_parent, k_lpanel,k_upanel);
-            
-            
-
             /*=======   Look ahead Panel Bcast  ======*/
             for(int_t k0_next=k0+1; k0_next<SUPERLU_MIN(nnodes, k0+1+numLA); k0_next++ )
             {
                 int k_next = perm_c_supno[k0_next];
-                /* if k_next's all children are factorized and 
-                I have not done panelBcast then perform it  */
                 if (!donePanelBcast[k0_next] &&
                 !localNumChildrenLeft[k0_next]
-                //  !gEtreeInfo->numChildLeft[k_next]
                  )
                 {
-                    /* code */
                     int offset_next = k0_next%numLA; 
                     dPanelBcast(k_next, offset_next);
                     donePanelBcast[k0_next] =1;
-                    
                 }
-                
             }
-
         } /*for k0= k_st:k_end */
-
     } /*for topoLvl = 0:maxTopoLevel*/
-
+    #endif 
     
     SUPERLU_FREE( donePanelBcast);
     SUPERLU_FREE( donePanelSolve);
