1d320527c9c3ff2be9a4a88b321c9afcd6695e2b
Piyush Sao <saopk@ornl.gov>
2022-03-07 10:46:33 -0500

    calling optimized omp code while superlu_acc_offload=0

--- SRC/pdgstrf.c
+++ SRC/pdgstrf.c
@@ -884,7 +884,7 @@ pdgstrf(superlu_dist_options_t * options, int m, int n, double anorm,
     {
         // if ( checkGPU(gpuHostMalloc((void**)&bigU,  bigu_size * sizeof(double), gpuHostMallocDefault)) )
         //     ABORT("Malloc fails for dgemm buffer U ");
-
+        stream_end_col = SUPERLU_MALLOC( nstreams * sizeof(int) );
         if ( !(bigU = doubleMalloc_dist(bigu_size)) )
         ABORT ("Malloc fails for dgemm U buffer");
         if ( !(bigV = doubleMalloc_dist(bigv_size)) )
@@ -1741,8 +1741,14 @@ pdgstrf(superlu_dist_options_t * options, int m, int n, double anorm,
 	/*******************************************************************/
 
 #ifdef GPU_ACC /*-- use GPU --*/
-	
+if(superlu_acc_offload)
+{
     #include "dSchCompUdt-gpu.c"
+}
+else 
+{
+    #include "dSchCompUdt-2Ddynamic.c"
+}
 	
 #else
 
