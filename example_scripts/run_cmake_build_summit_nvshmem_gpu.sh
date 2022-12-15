d4389141d9c1531c96976fcb59df34006e72dbc9
nanding0701 <dingnan0701@gmail.com>
2022-12-15 09:26:29 -0800

    add summit nvshmem compile and run scripts

--- example_scripts/run_cmake_build_summit_nvshmem_gpu.sh
+++ example_scripts/run_cmake_build_summit_nvshmem_gpu.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#module load netlib-lapack/3.8.0
-#module load gcc/6.4.0
-#module swap xl gcc
 module load xl
 module load cmake
 module load cuda
@@ -21,18 +18,12 @@ export MPI_HOME=$OLCF_SPECTRUM_MPI_ROOT
 export SHMEM_HOME=$MPI_HOME
 export NVSHMEM_MPI_LIB_NAME=libmpi_ibm.so
 export NVSHMEM_LMPI=-lmpi_ibm
-#export NVSHMEM_HOME=/ccs/home/nanding/mysoftware/nvshmem113_gdr_debug
-#export NVSHMEM_HOME=/ccs/home/nanding/mysoftware/nvshmem113_gdr
-export NVSHMEM_HOME=/ccs/home/nanding/mysoftware/nvshmem203_gdr_cuda1103/
+export NVSHMEM_HOME=/ccs/home/nanding/mysoftware/nvshmem270_gdr23_cuda1102_11232022
+#export NVSHMEM_HOME=/ccs/home/nanding/mysoftware/nvshmem203_gdr_cuda1103/
 export CUDA_INC=$CUDA_INC:$NVSHMEM_HOME/include
 
 export LD_LIBRARY_PATH=$NVSHMEM_HOME/lib:$LD_LIBRARY_PATH
-
-#CXX=g++
 CXX=mpiCC
-##-qsmp=omp
-#-Xcompiler -fPIC -shared -rdc=true
-#-DCUDA_SEPARABLE_COMPILATION
 cmake .. \
 	-DTPL_PARMETIS_INCLUDE_DIRS="${PARMETIS_ROOT}/include;${OLCF_CUDA_ROOT}/include" \
 	-DTPL_PARMETIS_LIBRARIES="${PARMETIS_ROOT}/lib/libparmetis.so;${PARMETIS_ROOT}/lib/libmetis.so" \
@@ -47,15 +38,10 @@ cmake .. \
 	-DTPL_ENABLE_CUDALIB=ON  \
 	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
 	-DCMAKE_CXX_FLAGS="-qsmp=omp -Ofast -DRELEASE ${INC_VTUNE}" \
-    	-DCMAKE_C_FLAGS="-qsmp=omp -std=c11 -DPRNTlevel=1 -DPROFlevel=0 -DDEBUGlevel=0 -DGPU_ACC -D_USE_NVSHMEM -fopenmp -I${NVSHMEM_HOME}/include/ " \
-    	-DCMAKE_CUDA_FLAGS="-ccbin ${CXX} -gencode arch=compute_70,code=sm_70 -I${NVSHMEM_HOME}/include/ -DENABLE_MPI_SUPPORT -DPRNTlevel=1 -std=c++11 -DPROFlevel=0 -DEBUGlevel=0 -DGPU_ACC --disable-warnings" \
-	-DCMAKE_EXE_LINKER_FLAGS="-lcuda -lcudadevrt -L${OLCF_CUDA_ROOT}/lib64 -lcudart_static -libverbs -L${NVSHMEM_HOME}/lib/ -lnvshmem -L${OLCF_SPECTRUM_MPI_ROOT}/lib -lmpi_ibm" 
+    	-DCMAKE_C_FLAGS="-qsmp=omp -std=c11 -DPRNTlevel=1 -DPROFlevel=0 -DDEBUGlevel=0 -DGPU_ACC -DSLU_HAVE_LAPACK -DGPU_SOLVE -D_USE_NVSHMEM -fopenmp -I${NVSHMEM_HOME}/include/ " \
+    	-DCMAKE_CUDA_FLAGS="-ccbin ${CXX} -gencode arch=compute_70,code=sm_70 -I${NVSHMEM_HOME}/include/ -DENABLE_MPI_SUPPORT -DPRNTlevel=1 -std=c++11 -DPROFlevel=0 -DEBUGlevel=0 -DGPU_ACC -DSLU_HAVE_LAPACK -DGPU_SOLVE --disable-warnings" \
+	-DCMAKE_EXE_LINKER_FLAGS="-lcuda -lcudadevrt -L${OLCF_CUDA_ROOT}/lib64 -lcudart_static -L${OLCF_CUDA_ROOT}/lib64/stubs/ -lnvidia-ml -libverbs -L${NVSHMEM_HOME}/lib/ -lnvshmem -L${OLCF_SPECTRUM_MPI_ROOT}/lib -lmpi_ibm" 
 make pddrive		
-#    	-DCMAKE_CUDA_FLAGS="-ccbin ${CXX} -gencode=arch=compute_70,code=sm_70 -G -Xcompiler -rdynamic -I$(OMPI_DIR)/include -I${NVSHMEM_HOME}/include/ -DPRNTlevel=1 -std=c++11 -DPROFlevel=0 -DEBUGlevel=0 -DGPU_ACC  --disable-warnings" \
-#	-DCMAKE_EXE_LINKER_FLAGS="-lcuda -L${CUDA_HOME}/lib64 -lcudart -libverbs -L${MPI_HOME}/lib ${MPI_LIBS} -L${NVSHMEM_HOME}/lib/ -lnvshmem -L${CUDA_HOME}/targets/ppc64le-linux/lib/stubs -lnvidia-ml" 
-#	-DCMAKE_CUDA_COMPILER=nvcc \
-#-L${OLCF_CUDA_ROOT}/lib64 -lcudadevrt	
-#    	-DCMAKE_CUDA_FLAGS="-DPRNTlevel=1 -std=c++11 -DPROFlevel=0 -DDEBUGlevel=0 -DGPU_ACC -gencode arch=compute_70,code=sm_70 -I${NVSHMEM_HOME}/include -I${OLCF_SPECTRUM_MPI_ROOT}/include -D_NVSHMEM_SHMEM_SUPPORT -D_NVSHMEM_MPI_SUPPORT -L${NVSHMEM_HOME}/lib/ -lnvshmem -L${OLCF_SPECTRUM_MPI_ROOT}/lib -lmpi_ibm -loshmem" 
 
 
 
