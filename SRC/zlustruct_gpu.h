195367595d7756d5ef3fa3269b0ca85394932e84
Jean-Luc Fattebert <fattebertj@ornl.gov>
2023-01-03 10:15:28 -0500

    Remove unused ptr and associated free

--- SRC/zlustruct_gpu.h
+++ SRC/zlustruct_gpu.h
@@ -95,8 +95,6 @@ typedef struct //LUstruct_gpu_
     local_u_blk_info_t *local_u_blk_infoVec;
 
     int_t *local_u_blk_infoPtr;
-    int_t *ijb_lookupVec;
-    int_t *ijb_lookupPtr;
 
     // GPU buffers for performing Schur Complement Update on GPU
     zSCUbuf_gpu_t scubufs[MAX_NGPU_STREAMS];
