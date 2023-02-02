bbffae9591f6d4a79162ece4cbc1d0c50825aa46
nanding0701 <42023341+nanding0701@users.noreply.github.com>
2023-02-02 09:58:24 -0800

    Update summit nvshmem build script

--- example_scripts/run_cmake_build_summit_ibm_nvshmem.sh
+++ example_scripts/run_cmake_build_summit_ibm_nvshmem.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#module load netlib-lapack/3.8.0
-#module load gcc/6.4.0
-#module swap xl gcc
 module load xl
 module load cmake/
 module load cuda/10.1.168
@@ -16,34 +13,31 @@ rm -rf CTestTestfile.cmake
 rm -rf cmake_install.cmake
 rm -rf DartConfiguration.tcl
 
-export CUDA_HOME=$OLCF_CUDA_ROOT
-export MPI_HOME=$OLCF_SPECTRUM_MPI_ROOT
-export SHMEM_HOME=$MPI_HOME
-export NVSHMEM_MPI_LIB_NAME=libmpi_ibm.so
-export NVSHMEM_LMPI=-lmpi_ibm
-export NVSHMEM_HOME=/ccs/home/nanding/mysoftware/EA025Release/nvshmem_0.2.5-0/install
+NVSHMEM_HOME=/ccs/home/nanding/mysoftware/nvshmem280_gdr23_cuda1103_20230127/
 export CUDA_INC=$CUDA_INC:$NVSHMEM_HOME/include
-
 export LD_LIBRARY_PATH=$NVSHMEM_HOME/lib:$LD_LIBRARY_PATH
-
 CXX=mpiCC
-##-qsmp=omp
 
 cmake .. \
-        -DTPL_PARMETIS_INCLUDE_DIRS="${PARMETIS_ROOT}/include;${OLCF_CUDA_ROOT}/include" \
-        -DTPL_PARMETIS_LIBRARIES="${PARMETIS_ROOT}/lib/libparmetis.so;${PARMETIS_ROOT}/lib/libmetis.so" \
-        -DTPL_CUDA_LIBRARIES="${OLCF_CUDA_ROOT}/lib64/libcublas.so;${OLCF_CUDA_ROOT}/lib64/libcusparse.so;${OLCF_CUDA_ROOT}/lib64/libcudart.so" \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DCMAKE_C_COMPILER=mpicc \
-        -DCMAKE_CXX_COMPILER=mpiCC \
-        -DTPL_ENABLE_CUDALIB=ON  \
-        -DCMAKE_INSTALL_PREFIX=. \
-        -DTPL_BLAS_LIBRARIES="/sw/summit/essl/6.1.0-2/essl/6.1/lib64/libessl.so;/sw/summit/essl/6.1.0-2/essl/6.1/lib64/libesslsmp.so" \
-        -DTPL_LAPACK_LIBRARIES="/sw/summit/essl/6.1.0-2/essl/6.1/lib64/libessl.so" \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-        -DCMAKE_CXX_FLAGS="-qsmp=omp -Ofast -DRELEASE ${INC_VTUNE}" \
-        -DCMAKE_C_FLAGS="-qsmp=omp -std=c11 -DPRNTlevel=1 -DPROFlevel=0 -DDxEBUGlevel=0 -DGPU_ACC -DHAVE_CUDA -fopenmp -I${NVSHMEM_HOME}/include/ " \
-        -DCMAKE_CUDA_FLAGS="-ccbin ${CXX} -gencode=arch=compute_70,code=sm_70 -G -Xcompiler -rdynamic -I${NVSHMEM_HOME}/include/ -DPRNTlevel=1 -std=c++11 -DPROFlevel=0 -DEBUGlevel=0 -DGPU_ACC -DHAVE_CUDA --disable-warnings" \
-        -DCMAKE_EXE_LINKER_FLAGS="-lcuda -L${CUDA_HOME}/lib64 -lcudart -libverbs -L${MPI_HOME}/lib ${MPI_LIBS} -L${NVSHMEM_HOME}/lib/ -lnvshmem"
+  -DCMAKE_C_FLAGS="-DGPU_SOLVE -qsmp=omp -DSLU_HAVE_LAPACK -fopenmp -std=c11 -DPRNTlevel=1 -DPROFlevel=0 -DDEBUGlevel=0 -DAdd_ -I${NVSHMEM_HOME}/include" \
+  -DCMAKE_CXX_COMPILER=mpiCC \
+  -DCMAKE_C_COMPILER=mpicc \
+  -DTPL_ENABLE_INTERNAL_BLASLIB=OFF \
+  -DTPL_ENABLE_LAPACKLIB=ON \
+  -DBUILD_SHARED_LIBS=ON \
+  -DTPL_ENABLE_CUDALIB=ON \
+  -DCMAKE_CUDA_FLAGS="-I${NVSHMEM_HOME}/include -I${MPICH_DIR}/include -ccbin=${CXX}" \
+  -DCMAKE_CUDA_ARCHITECTURES=70 \
+  -DCMAKE_INSTALL_PREFIX=. \
+  -DCMAKE_INSTALL_LIBDIR=./lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DTPL_BLAS_LIBRARIES="${OLCF_ESSL_ROOT}/lib64/libessl.so;${OLCF_ESSL_ROOT}/lib64/libesslsmp.so" \
+  -DTPL_LAPACK_LIBRARIES="${OLCF_ESSL_ROOT}/lib64/libessl.so" \
+  -DTPL_PARMETIS_INCLUDE_DIRS="${PARMETIS_ROOT}/include;${OLCF_CUDA_ROOT}/include" \
+  -DTPL_PARMETIS_LIBRARIES="${PARMETIS_ROOT}/lib/libparmetis.so;${PARMETIS_ROOT}/lib/libmetis.so" \
+  -DTPL_ENABLE_COMBBLASLIB=OFF \
+  -DTPL_ENABLE_NVSHMEM=ON \
+  -DTPL_NVSHMEM_LIBRARIES="-lcuda -lcudadevrt -L${OLCF_CUDA_ROOT}/lib64 -lcudart_static -L${OLCF_CUDA_ROOT}/lib64/stubs/ -lnvidia-ml -libverbs -L${NVSHMEM_HOME}/lib/ -lnvshmem -L${OLCF_SPECTRUM_MPI_ROOT}/lib -lmpi_ibm"
+  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+
 make pddrive
