11217d3ac69e93897f56339d13068100bd880f3d
Yang Liu <liuyangzhuan@gmail.com>
2023-07-27 09:23:17 -0700

    fixing taskloop error for nvhpc

--- SRC/pdgstrs_lsum.c
+++ SRC/pdgstrs_lsum.c
@@ -528,7 +528,9 @@ void dlsum_fmod_inv
 #pragma	omp	parallel for private (lptr1,luptr1,nlb1,thread_id1,lsub1,lusup1,nsupr1,Linv,nn,lbstart,lbend,luptr_tmp1,nbrow,lb,lptr1_tmp,rtemp_loc,nbrow_ref,lptr,nbrow1,ik,rel,lk,iknsupc,il,i,irow,fmod_tmp,ikcol,p,ii,jj,t1,t2,j,nleaf_send_tmp)
 #else
 // This taskloop causes code to crash or generate wrong solution for some intel compilers
+#if defined __GNUC__  && !defined __NVCOMPILER
 #pragma	omp	taskloop private (lptr1,luptr1,nlb1,thread_id1,lsub1,lusup1,nsupr1,Linv,nn,lbstart,lbend,luptr_tmp1,nbrow,lb,lptr1_tmp,rtemp_loc,nbrow_ref,lptr,nbrow1,ik,rel,lk,iknsupc,il,i,irow,fmod_tmp,ikcol,p,ii,jj,t1,t2,j,nleaf_send_tmp) untied nogroup
+#endif
 #endif
 #endif
 			for (nn=0;nn<Nchunk;++nn){
@@ -1068,7 +1070,9 @@ void dlsum_fmod_inv_master
 			remainder = nlb % Nchunk;
 
 #ifdef _OPENMP
+#if defined __GNUC__  && !defined __NVCOMPILER
 #pragma	omp	taskloop private (lptr1,luptr1,nlb1,thread_id1,lsub1,lusup1,nsupr1,Linv,nn,lbstart,lbend,luptr_tmp1,nbrow,lb,lptr1_tmp,rtemp_loc,nbrow_ref,lptr,nbrow1,ik,rel,lk,iknsupc,il,i,irow,fmod_tmp,ikcol,p,ii,jj,t1,t2,j) untied
+#endif
 #endif
 			for (nn=0;nn<Nchunk;++nn){
 
@@ -1436,7 +1440,9 @@ void dlsum_bmod_inv
 #pragma	omp	parallel for private (thread_id1,Uinv,nn,lbstart,lbend,ub,temp,rtemp_loc,ik,lk1,gik,gikcol,usub,uval,lsub,lusup,iknsupc,il,i,irow,bmod_tmp,p,ii,jj,t1,t2,j,ikfrow,iklrow,dest,y,uptr,fnz,nsupr)
 #else
 // This taskloop causes code to crash or generate wrong solution for some intel compilers
+#if defined __GNUC__  && !defined __NVCOMPILER
 #pragma	omp	taskloop firstprivate (stat) private (thread_id1,Uinv,nn,lbstart,lbend,ub,temp,rtemp_loc,ik,lk1,gik,gikcol,usub,uval,lsub,lusup,iknsupc,il,i,irow,bmod_tmp,p,ii,jj,t1,t2,j,ikfrow,iklrow,dest,y,uptr,fnz,nsupr,nroot_send_tmp) untied nogroup
+#endif
 #endif
 #endif
 		for (nn=0;nn<Nchunk;++nn){
