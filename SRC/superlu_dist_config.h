3fe6856d49252ddc2edd81a2ef0cd6092e144696
Sherry Li <xsli@lbl.gov>
2023-11-07 22:52:03 -0800

    Add target_compile_features() for superlu_dist target.

--- SRC/superlu_dist_config.h
+++ SRC/superlu_dist_config.h
@@ -1,8 +1,26 @@
-#define XSDK_INDEX_SIZE 64
-/* #define SLU_HAVE_LAPACK TRUE */
+/* superlu_dist_config.h.in */
+
+/* Enable CUDA */
+/* #undef HAVE_CUDA */
+
+/* Enable HIP */
+/* #undef HAVE_HIP */
+
+/* Enable parmetis */
 #define HAVE_PARMETIS TRUE
-/* #define HAVE_COLAMD TRUE */
-/* #define HAVE_COMBBLAS TRUE */
-#if (XSDK_INDEX_SIZE == 64)
+
+/* Enable colamd */
+/* #undef HAVE_COLAMD */
+
+/* Enable LAPACK */
+/* #undef SLU_HAVE_LAPACK */
+
+/* Enable CombBLAS */
+/* #undef HAVE_COMBBLAS */
+
+/* enable 64bit index mode */
+#define XSDK_INDEX_SIZE 64
+
+#if defined(XSDK_INDEX_SIZE) && (XSDK_INDEX_SIZE == 64)
 #define _LONGINT 1
 #endif
