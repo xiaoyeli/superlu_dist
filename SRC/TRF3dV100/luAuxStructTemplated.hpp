d9d90d8c9fa1d6bd11c3c77c31c1bca55e0f67dd
Piyush Sao <saopk@ornl.gov>
2023-11-19 14:35:49 -0500

    Added new template aliases for different data types, including AnormType, trf3dpartitionType, LUStruct_type, LocalLU_type, LUValSubBuf_type, and diagFactBufs_type. These aliases improve code readability

--- SRC/TRF3dV100/luAuxStructTemplated.hpp
+++ SRC/TRF3dV100/luAuxStructTemplated.hpp
@@ -1,5 +1,6 @@
 #pragma once 
 #include <mpi.h>
+#include "superlu_defs.h"
 template<typename Ftype>
 struct diagFactBufs_t {
     Ftype* BlockLFactor;
@@ -43,5 +44,90 @@ MPI_Datatype get_mpi_type<doublecomplex>()
     return MPI_C_DOUBLE_COMPLEX;
 }
 
+
+
+template <typename Ftype>
+using AnormType = typename std::conditional<
+    std::is_same<Ftype, complex>::value || std::is_same<Ftype, double complex>::value,
+    float,
+    double
+>::type;
+
+template <typename Ftype>
+using trf3dpartitionType 
+template <typename Ftype>
+using trf3dpartitionType = typename std::conditional<
+    std::is_same<Ftype, double>::value,
+    dtrf3dpartition_t,
+    typename std::conditional<
+        std::is_same<Ftype, float>::value,
+        strf3dpartition_t,
+        typename std::conditional<
+            std::is_same<Ftype, complex>::value,
+            ctrf3dpartition_t,
+            ztrf3dpartition_t
+        >::type
+    >::type
+>::type;
+
+template <typename Ftype>
+using LUStruct_type = typename std::conditional<
+    std::is_same<Ftype, double>::value,
+    dLUstruct_t,
+    typename std::conditional<
+        std::is_same<Ftype, float>::value,
+        sLUstruct_t,
+        typename std::conditional<
+            std::is_same<Ftype, complex>::value,
+            cLUstruct_t,
+            zLUstruct_t
+        >::type
+    >::type
+>::type;
+
+template <typename Ftype>
+using LocalLU_type = typename std::conditional<
+    std::is_same<Ftype, double>::value,
+    dLocalLU_t,
+    typename std::conditional<
+        std::is_same<Ftype, float>::value,
+        sLocalLU_t,
+        typename std::conditional<
+            std::is_same<Ftype, complex>::value,
+            cLocalLU_t,
+            zLocalLU_t
+        >::type
+    >::type
+>::type;
+
+template <typename Ftype>
+using LUValSubBuf_type = typename std::conditional<
+    std::is_same<Ftype, double>::value,
+    dLUValSubBuf_t,
+    typename std::conditional<
+        std::is_same<Ftype, float>::value,
+        sLUValSubBuf_t,
+        typename std::conditional<
+            std::is_same<Ftype, complex>::value,
+            cLUValSubBuf_t,
+            zLUValSubBuf_t
+        >::type
+    >::type
+>::type;
+
+template <typename Ftype>
+using diagFactBufs_type = typename std::conditional<
+    std::is_same<Ftype, double>::value,
+    ddiagFactBufs_t,
+    typename std::conditional<
+        std::is_same<Ftype, float>::value,
+        sdiagFactBufs_t,
+        typename std::conditional<
+            std::is_same<Ftype, complex>::value,
+            cdiagFactBufs_t,
+            zdiagFactBufs_t
+        >::type
+    >::type
+>::type;
 
 
