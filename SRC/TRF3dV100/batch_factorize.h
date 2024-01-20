85041817e3b02b907f3dfb4dc0c88400cfc8fbbd
Wajih Boukaram <wajih.boukaram@gmail.com>
2024-01-19 18:28:57 -0800

    Copied over batch factorization code from branch gpu3d-batch and disabled old code in the c++ templated code.

---
+++ SRC/TRF3dV100/batch_factorize.h
@@ -0,0 +1,89 @@
+#ifndef __SUPERLU_BATCH_FACTORIZE_H__
+#define __SUPERLU_BATCH_FACTORIZE_H__
+
+#include "superlu_ddefs.h"
+#include "superlu_summit.h"
+
+#ifdef HAVE_MAGMA
+#include "magma.h"
+#endif
+// Device memory used to store marshalled batch data for LU and TRSM
+struct BatchLUMarshallData 
+{
+    BatchLUMarshallData();
+    ~BatchLUMarshallData();
+
+    // Diagonal device pointer data 
+    double **dev_diag_ptrs;
+    int_t *dev_diag_ld_array, *dev_diag_dim_array, *dev_info_array;
+    
+    // TRSM panel device pointer data 
+    double **dev_panel_ptrs;
+    int_t *dev_panel_ld_array, *dev_panel_dim_array;
+
+    // Max of marshalled device data 
+    int_t max_panel, max_diag;
+
+    // Number of marshalled operations
+    int_t batchsize;
+
+    void setBatchSize(int_t batch_size);
+};
+
+// Device memory used to store marshalled batch data for Schur complement update 
+struct BatchSCUMarshallData 
+{
+    BatchSCUMarshallData();
+    ~BatchSCUMarshallData();
+
+    // GEMM device pointer data 
+    double **dev_A_ptrs, **dev_B_ptrs, **dev_C_ptrs;
+    int_t *dev_lda_array, *dev_ldb_array, *dev_ldc_array;
+    int_t *dev_m_array, *dev_n_array, *dev_k_array;
+
+    // Panel device pointer data and scu loop limits 
+    int_t* dev_ist, *dev_iend, *dev_jst, *dev_jend;
+    
+    // Max of marshalled gemm device data 
+    int_t max_m, max_n, max_k;    
+    
+    // Max of marshalled loop limits  
+    int_t max_ilen, max_jlen;
+
+    // Number of marshalled operations
+    int_t batchsize;
+
+    void setBatchSize(int_t batch_size);
+};
+
+struct BatchFactorizeWorkspace {
+    // Library handles 
+#ifdef HAVE_MAGMA    
+    magma_queue_t magma_queue;
+#endif
+    cudaStream_t stream;
+    cublasHandle_t cuhandle;
+    
+    // Marshall data 
+    BatchLUMarshallData marshall_data;
+    BatchSCUMarshallData sc_marshall_data;
+
+    // GPU copy of the supernode data
+    int_t* perm_c_supno, *xsup;
+    int_t maxSuperSize, ldt, nsupers;
+
+    // GPU copy of the local LU data 
+    dLocalLU_t d_localLU;
+
+    // GPU buffers for the SCU gemms 
+    double** gemm_buff_ptrs, *gemm_buff_base;
+    int64_t *gemm_buff_offsets;
+
+    // Copy of the lower panel index data in a more parallel friendly format 
+    int_t *d_lblock_gid_dat, **d_lblock_gid_ptrs;
+    int_t *d_lblock_start_dat, **d_lblock_start_ptrs;
+    int64_t *d_lblock_gid_offsets, *d_lblock_start_offsets;
+    int64_t total_l_blocks, total_start_size;
+};
+
+#endif 
---
+++ SRC/TRF3dV100/batch_factorize_marshall.h
@@ -0,0 +1,283 @@
+#ifndef __SUPERLU_BATCH_FACTORIZE_MARSHALL_H__
+#define __SUPERLU_BATCH_FACTORIZE_MARSHALL_H__
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Marshall Functors for batched execution 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "superlu_defs.h"
+
+struct MarshallLUFunc_flat {
+    int_t k_st, *ld_batch, *dim_batch;
+    double** diag_ptrs, **Lnzval_bc_ptr;
+    int_t** Lrowind_bc_ptr, *xsup, *dperm_c_supno;
+    
+    MarshallLUFunc_flat(
+        int_t k_st, double** diag_ptrs, int_t *ld_batch, int_t *dim_batch, double** Lnzval_bc_ptr,
+        int_t** Lrowind_bc_ptr, int_t *dperm_c_supno, int_t *xsup
+    )
+    {
+        this->k_st = k_st;
+        this->ld_batch = ld_batch;
+        this->dim_batch = dim_batch;
+        this->diag_ptrs = diag_ptrs;
+
+        this->Lnzval_bc_ptr = Lnzval_bc_ptr;
+        this->Lrowind_bc_ptr = Lrowind_bc_ptr;
+        this->dperm_c_supno = dperm_c_supno;
+        this->xsup = xsup;
+    }
+    
+    __device__ void operator()(const int_t &i) const
+    {   
+        int_t k = dperm_c_supno[k_st + i];
+        int_t *Lrowind_bc = Lrowind_bc_ptr[k];
+        double* Lnzval = Lnzval_bc_ptr[k];
+
+        if(Lnzval && Lrowind_bc)
+        {
+            diag_ptrs[i] = Lnzval;
+            ld_batch[i] = Lrowind_bc[1];
+            dim_batch[i] = SuperSize(k);
+        }
+        else
+        {
+            diag_ptrs[i] = NULL;
+            ld_batch[i] = 1;
+            dim_batch[i] = 0;
+        }
+    }
+};
+
+struct MarshallTRSMUFunc_flat {
+    int_t k_st, *diag_ld_batch, *diag_dim_batch, *panel_ld_batch, *panel_dim_batch;
+    double** diag_ptrs, **panel_ptrs, **Unzval_br_new_ptr, **Lnzval_bc_ptr;
+    int_t** Ucolind_br_ptr, **Lrowind_bc_ptr, *xsup, *dperm_c_supno;
+
+    MarshallTRSMUFunc_flat(
+        int_t k_st, double** diag_ptrs, int_t *diag_ld_batch, int_t *diag_dim_batch, double** panel_ptrs,
+        int_t *panel_ld_batch, int_t *panel_dim_batch, double **Unzval_br_new_ptr, int_t** Ucolind_br_ptr, 
+        double** Lnzval_bc_ptr, int_t** Lrowind_bc_ptr, int_t *dperm_c_supno, int_t *xsup
+    )
+    {
+        this->k_st = k_st;
+        this->diag_ptrs = diag_ptrs;
+        this->diag_ld_batch = diag_ld_batch;
+        this->diag_dim_batch = diag_dim_batch;
+        this->panel_ptrs = panel_ptrs;
+        this->panel_ld_batch = panel_ld_batch;
+        this->panel_dim_batch = panel_dim_batch;
+        this->Unzval_br_new_ptr = Unzval_br_new_ptr;
+        this->Ucolind_br_ptr = Ucolind_br_ptr; 
+        this->Lnzval_bc_ptr = Lnzval_bc_ptr;
+        this->Lrowind_bc_ptr = Lrowind_bc_ptr;
+        this->dperm_c_supno = dperm_c_supno;
+        this->xsup = xsup;
+    }
+    
+    __device__ void operator()(const int_t &i) const
+    {   
+        int_t k = dperm_c_supno[k_st + i];
+        int_t ksupc = SuperSize(k);
+
+        int_t *Ucolind_br = Ucolind_br_ptr[k];
+        double* Unzval = Unzval_br_new_ptr[k];
+        int_t *Lrowind_bc = Lrowind_bc_ptr[k];
+        double* Lnzval = Lnzval_bc_ptr[k];
+
+        if(Ucolind_br && Unzval && Lrowind_bc && Lnzval)
+        {
+            panel_ptrs[i] = Unzval;
+            panel_ld_batch[i] = Ucolind_br[2];
+            panel_dim_batch[i] = Ucolind_br[1];
+            diag_ptrs[i] = Lnzval;
+            diag_ld_batch[i] = Lrowind_bc[1];
+            diag_dim_batch[i] = ksupc;
+        }
+        else
+        {
+            panel_ptrs[i] = diag_ptrs[i] = NULL;
+            panel_ld_batch[i] = diag_ld_batch[i] = 1;
+            panel_dim_batch[i] = diag_dim_batch[i] = 0;
+        }    
+    }
+};
+
+struct MarshallTRSMLFunc_flat {
+    int_t k_st, *diag_ld_batch, *diag_dim_batch, *panel_ld_batch, *panel_dim_batch;
+    double** diag_ptrs, **panel_ptrs, **Lnzval_bc_ptr;
+    int_t** Lrowind_bc_ptr, *xsup,  *dperm_c_supno;
+
+    MarshallTRSMLFunc_flat(
+        int_t k_st, double** diag_ptrs, int_t *diag_ld_batch, int_t *diag_dim_batch, double** panel_ptrs,
+        int_t *panel_ld_batch, int_t *panel_dim_batch, double** Lnzval_bc_ptr, int_t** Lrowind_bc_ptr, 
+        int_t *dperm_c_supno, int_t *xsup
+    )
+    {
+        this->k_st = k_st;
+        this->diag_ptrs = diag_ptrs;
+        this->diag_ld_batch = diag_ld_batch;
+        this->diag_dim_batch = diag_dim_batch;
+        this->panel_ptrs = panel_ptrs;
+        this->panel_ld_batch = panel_ld_batch;
+        this->panel_dim_batch = panel_dim_batch;
+
+        this->Lnzval_bc_ptr = Lnzval_bc_ptr;
+        this->Lrowind_bc_ptr = Lrowind_bc_ptr;
+        this->dperm_c_supno = dperm_c_supno;
+        this->xsup = xsup;
+    }
+    
+    __device__ void operator()(const int_t &i) const
+    {
+        int_t k = dperm_c_supno[k_st + i];
+        int_t ksupc = SuperSize(k);
+        int_t *Lrowind_bc = Lrowind_bc_ptr[k];
+        double* Lnzval = Lnzval_bc_ptr[k];
+
+        if(Lnzval && Lrowind_bc)
+        {
+            int_t diag_block_offset = Lrowind_bc[BC_HEADER + 1];
+            int_t nzrows = Lrowind_bc[1];
+            int_t len = nzrows - diag_block_offset;
+
+            panel_ptrs[i] = Lnzval + diag_block_offset;
+            panel_ld_batch[i] = nzrows;
+            panel_dim_batch[i] = len;
+            diag_ptrs[i] = Lnzval;
+            diag_ld_batch[i] = nzrows;
+            diag_dim_batch[i] = ksupc;
+        }
+        else
+        {
+            panel_ptrs[i] = diag_ptrs[i] = NULL;
+            panel_ld_batch[i] = diag_ld_batch[i] = 1;
+            panel_dim_batch[i] = diag_dim_batch[i] = 0;
+        }    
+    }
+};
+
+struct MarshallSCUFunc_flat {
+    double** A_ptrs, **B_ptrs, **C_ptrs;
+    int_t* lda_array, *ldb_array, *ldc_array, *m_array, *n_array, *k_array;
+    double **Unzval_br_new_ptr, **Lnzval_bc_ptr, **dgpuGemmBuffs;
+    int_t** Ucolind_br_ptr, **Lrowind_bc_ptr, *xsup, *dperm_c_supno, k_st;
+    int_t *ist, *iend, *jst, *jend;
+
+    MarshallSCUFunc_flat(
+        int_t k_st, double** A_ptrs, int_t* lda_array, double** B_ptrs, int_t* ldb_array, double **C_ptrs, int_t *ldc_array,
+        int_t *m_array, int_t *n_array, int_t *k_array, int_t *ist, int_t *iend, int_t *jst, int_t *jend, 
+        double **Unzval_br_new_ptr, int_t** Ucolind_br_ptr, double** Lnzval_bc_ptr, int_t** Lrowind_bc_ptr, 
+        int_t *dperm_c_supno, int_t *xsup, double** dgpuGemmBuffs
+    )
+    {
+        this->k_st = k_st;
+        this->A_ptrs = A_ptrs;
+        this->B_ptrs = B_ptrs;
+        this->C_ptrs = C_ptrs;
+        this->lda_array = lda_array;
+        this->ldb_array = ldb_array;
+        this->ldc_array = ldc_array;
+        this->m_array = m_array;
+        this->n_array = n_array;
+        this->k_array = k_array;
+        this->ist = ist;
+        this->iend = iend;
+        this->jst = jst;
+        this->jend = jend;
+        this->Unzval_br_new_ptr = Unzval_br_new_ptr;
+        this->Ucolind_br_ptr = Ucolind_br_ptr; 
+        this->Lnzval_bc_ptr = Lnzval_bc_ptr;
+        this->Lrowind_bc_ptr = Lrowind_bc_ptr;
+        this->dperm_c_supno = dperm_c_supno;
+        this->xsup = xsup;
+        this->dgpuGemmBuffs = dgpuGemmBuffs;
+    }
+
+    __device__ void operator()(const int_t &i) const
+    {
+        int_t k = dperm_c_supno[k_st + i];
+        
+        int_t *Ucolind_br = Ucolind_br_ptr[k];
+        double* Unzval = Unzval_br_new_ptr[k];
+        int_t *Lrowind_bc = Lrowind_bc_ptr[k];
+        double* Lnzval = Lnzval_bc_ptr[k];
+
+        if(Ucolind_br && Unzval && Lrowind_bc && Lnzval)
+        {
+            int_t diag_block_offset = Lrowind_bc[BC_HEADER + 1];
+            int_t L_nzrows = Lrowind_bc[1];
+            int_t L_len = L_nzrows - diag_block_offset;
+
+            A_ptrs[i] = Lnzval + diag_block_offset;
+            B_ptrs[i] = Unzval;
+            C_ptrs[i] = dgpuGemmBuffs[i];
+
+            lda_array[i] = L_nzrows;
+            ldb_array[i] = Ucolind_br[2];
+            ldc_array[i] = L_len;
+                        
+            m_array[i] = ldc_array[i];
+            n_array[i] = Ucolind_br[1];
+            k_array[i] = SuperSize(k);
+
+            ist[i] = 1;
+            jst[i] = 0;
+            iend[i] = Lrowind_bc[0];
+            jend[i] = Ucolind_br[0];
+        }
+        else
+        {
+            A_ptrs[i] = B_ptrs[i] = C_ptrs[i] = NULL;
+            lda_array[i] = ldb_array[i] = ldc_array[i] = 1;
+            m_array[i] = n_array[i] = k_array[i] = 0;
+        }
+    }
+};
+
+template <class T, class offT>
+struct UnaryOffsetPtrAssign
+{
+	T *base_mem, **ptrs;
+	offT* offsets;
+	
+    UnaryOffsetPtrAssign(T *base_mem, offT* offsets, T **ptrs)
+	{
+		this->base_mem = base_mem;
+		this->offsets = offsets;
+		this->ptrs = ptrs;
+	}
+	
+    inline __host__ __device__ void operator()(const offT &index) const
+    {
+        ptrs[index] = (offsets[index] < 0 ? NULL : base_mem + offsets[index]);
+    }
+};
+
+template<class T, class offT>
+inline void generateOffsetPointers(T *base_mem, offT *offsets, T **ptrs, size_t num_arrays)
+{
+    UnaryOffsetPtrAssign<T, offT> offset_ptr_functor(base_mem, offsets, ptrs);
+
+    thrust::for_each(
+        thrust::system::cuda::par, thrust::counting_iterator<offT>(0),
+        thrust::counting_iterator<offT>(num_arrays), offset_ptr_functor
+    );
+}
+
+template<typename T>
+struct element_diff : public thrust::unary_function<T,T>
+{
+    T* st, *end;
+    element_diff(T* st, T *end) 
+    {
+        this->st = st;
+        this->end = end;
+    }
+    
+    __device__ T operator()(const T &x) const
+    {
+        return end[x] - st[x];
+    }
+};
+
+#endif 
