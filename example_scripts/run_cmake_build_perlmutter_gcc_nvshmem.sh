a82e26152e4634d9219d07eef3393fb8b223c1aa
SidShi <shitianyisid@gmail.com>
2026-03-17 14:51:14 -0700

    Fixing small EXAMPLE Cmake vbatch error and update perlmutter compilation script

--- example_scripts/run_cmake_build_perlmutter_gcc_nvshmem.sh
+++ example_scripts/run_cmake_build_perlmutter_gcc_nvshmem.sh
@@ -33,19 +33,21 @@ module load cmake
 module load cudatoolkit
 # avoid bug in cray-libsci/21.08.1.2
 # module load cray-libsci/22.11.1.2
-module load cray-libsci/24.07.0
+# module load cray-libsci/24.07.0
+# module load cray-libsci/25.09.0
 # module use /global/common/software/nersc/pe/modulefiles/latest
 # module load nvshmem/2.11.0
 export MAGMA_ROOT=/global/cfs/cdirs/m2957/lib/magma_master
 # avoid bug in cudatoolkit
 # export LD_LIBRARY_PATH=${LD_LIBRARY_PATH//\/usr\/local\/cuda-12.4\/compat:/}
 # export LD_LIBRARY_PATH=${LD_LIBRARY_PATH//\/usr\/local\/cuda-11.7\/compat:/}
+# module load gcc-native/13.2
 
 NVSHMEM_HOME=/global/cfs/cdirs/m2957/lib/lib/PrgEnv-gnu/nvshmem_src_2.8.0-3/build/
 #NVSHMEM_HOME=${CRAY_NVIDIA_PREFIX}/comm_libs/nvshmem/
 cmake .. \
   -DCMAKE_C_FLAGS="-O2 -std=c11 -DPRNTlevel=1 -DPROFlevel=1 -DDEBUGlevel=0 -DAdd_" \
-  -DCMAKE_CXX_FLAGS="-O2 -std=c++11" \
+  -DCMAKE_CXX_FLAGS="-O2 -std=c++14" \
   -DCMAKE_Fortran_FLAGS="-O2" \
   -DCMAKE_CXX_COMPILER=CC \
   -DCMAKE_C_COMPILER=cc \
@@ -55,8 +57,9 @@ cmake .. \
   -DTPL_ENABLE_LAPACKLIB=ON \
   -DBUILD_SHARED_LIBS=OFF \
   -DTPL_ENABLE_CUDALIB=ON \
-  -DCMAKE_CUDA_FLAGS="-I${NVSHMEM_HOME}/include -I${MPICH_DIR}/include -ccbin=CC" \
+  -DCMAKE_CUDA_FLAGS="-I${MPICH_DIR}/include -ccbin=CC" \
   -DCMAKE_CUDA_ARCHITECTURES=80 \
+  -DCMAKE_CUDA_STANDARD=14 \
   -DCMAKE_INSTALL_PREFIX=. \
   -DCMAKE_INSTALL_LIBDIR=./lib \
   -DCMAKE_BUILD_TYPE=Debug \
@@ -68,7 +71,7 @@ cmake .. \
   -DTPL_PARMETIS_INCLUDE_DIRS="/global/cfs/cdirs/m2957/lib/lib/PrgEnv-gnu/parmetis-4.0.3/include;/global/cfs/cdirs/m2957/lib/lib/PrgEnv-gnu/parmetis-4.0.3/metis/include" \
   -DTPL_PARMETIS_LIBRARIES="/global/cfs/cdirs/m2957/lib/lib/PrgEnv-gnu/parmetis-4.0.3/build/Linux-x86_64/libparmetis/libparmetis.so;/global/cfs/cdirs/m2957/lib/lib/PrgEnv-gnu/parmetis-4.0.3/build/Linux-x86_64/libmetis/libmetis.so" \
   -DTPL_ENABLE_COMBBLASLIB=OFF \
-  -DTPL_ENABLE_NVSHMEM=ON \
+  -DTPL_ENABLE_NVSHMEM=OFF \
   -DTPL_NVSHMEM_LIBRARIES="-L${CUDA_HOME}/lib64/stubs/ -lnvidia-ml -L/usr/lib64 -lgdrapi -lstdc++ -L/opt/cray/libfabric/1.22.0/lib64 -lfabric -L${NVSHMEM_HOME}/lib -lnvshmem" \
   -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
   -DMPIEXEC_NUMPROC_FLAG=-n \
@@ -78,6 +81,7 @@ cmake .. \
 # make pddrive -j16
 # make pddrive3d -j16
 make pddrive3d_vbatch -j16
+# make pddrive3d_vbatch
 # make pzdrive3d -j16
 # make pzdrive
 # make f_pddrive
